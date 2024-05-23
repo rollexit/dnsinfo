aa191ca78c4bdad1502aaf5bb03c0fd736c4997c
unknown <rolletindjiete@outlook.com>
2024-05-23 09:34:08 +0000

    first commit

---
+++ dns.py
@@ -0,0 +1,17 @@
+import whois
+
+def get_domain_info(domain_name):
+    try:
+        domain_info = whois.whois(domain_name)
+        return domain_info
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+if __name__ == "__main__":
+    domain_name = "rollexdigital.cloud"
+    domain_info = get_domain_info(domain_name)
+    if isinstance(domain_info, str):
+        print(domain_info)
+    else:
+        for key, value in domain_info.items():
+            print(f"{key}: {value}")
